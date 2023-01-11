6c3464e54b5e14cc3eeb1310b31dda0ac47ad123
Adrian Gallano <adriangallanoengr@gmail.com>
2023-01-11 22:52:46 +0800

    Add style and structure

---
+++ scripts/to-do-list.js
@@ -0,0 +1,14 @@
+/* 
+
+features:
+we can use array to store the current values
+and use unordered list for the GUI
+
+Add -> create and append
+Edit -> I need to get the current id or reference of that current data
+Delete -> node.remove()
+Clear -> node.remove()?? or just clear it using innerHtml
+
+*/
+
+
