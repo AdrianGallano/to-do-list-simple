596bb42afc0209522bc005822f09d7d8653de1d8
Adrian Gallano <adriangallanoengr@gmail.com>
2023-01-12 16:10:33 +0800

    Add add, delete, and done feature

--- scripts/to-do-list.js
+++ scripts/to-do-list.js
@@ -11,4 +11,71 @@ Clear -> node.remove()?? or just clear it using innerHtml
 
 */
 
+const tasks = document.getElementsByClassName("task")
+const deleteTasks = document.getElementsByClassName("task-delete")
+const createTask = document.getElementById("add-task")
+const taskContainer = document.querySelector(".tasks")
 
+const changeTaskState = (task) => {
+    if(!task.classList.contains("task-cross"))
+        task.classList.add("task-cross")
+    else
+        task.classList.remove("task-cross")
+}
+
+const deleteTask = (task) => {
+    task.parentNode.parentNode.remove()
+}
+
+const addTask = () => {
+    // creating
+    let newTaskContent = prompt("Please Add a new task: ");
+    let listData = document.createElement("li")
+    let paraData = document.createElement("p")
+    let divData = document.createElement("div")
+    let buttonEdit  = document.createElement("button")
+    let buttonDelete  = document.createElement("button")
+    
+    listData.classList.add("task")
+    paraData.setAttribute("data-state", "task-state")
+    divData.classList.add("task-selections")
+    buttonEdit.classList.add("task-edit")
+    buttonDelete.classList.add("task-delete")
+    buttonEdit.textContent = "Edit"
+    buttonDelete.textContent = "Delete"
+
+    divData.append(buttonEdit)
+    divData.append(buttonDelete)
+    paraData.textContent = newTaskContent
+    listData.append(paraData)
+    listData.append(divData)
+    
+    taskContainer.append(listData)
+
+    buttonDelete.addEventListener("click", (e)=>{
+        deleteTask(e.target)
+    })
+    paraData.addEventListener("click", (e)=>{
+        changeTaskState(e.target)
+    })
+}
+
+
+createTask.addEventListener("click", addTask)
+
+
+const start = () => {
+    Array.from(tasks).forEach((task) => {
+        task.children[0].addEventListener("click", (e)=>{
+            changeTaskState(e.target)
+        })
+    })
+    
+    Array.from(deleteTasks).forEach((task) => {
+        task.addEventListener("click", (e) => {
+            deleteTask(e.target)
+        })
+    })
+}
+
+start()
